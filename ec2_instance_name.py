cf1d94393e2a04a928f1b5d63a00648ec87d1ae6
Vineet Gupta <vineet.scm@gmail.com>
2017-03-07 15:36:29 +0400

    python api for aws

---
+++ ec2_instance_name.py
@@ -0,0 +1,10 @@
+import boto3
+
+def get_instance_name(fid):
+    ec2 = boto3.resource('ec2')
+    ec2instance = ec2.Instance(fid)
+    instancename = ''
+    for tags in ec2instance.tags:
+        if tags["Key"] == 'Name':
+            instancename = tags["Value"]
+    return instancename
